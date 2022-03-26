34a564722cea0839b4100f01a4372890c1778cc6
Jaanonim <mat8mro@gmail.com>
2022-03-26 22:28:31 +0100

    addding environment check in config

--- client/src/Config/config.ts
+++ client/src/Config/config.ts
@@ -1,3 +1,10 @@
-export default {
-    apiUrl: 'http://localhost:5000/api'
-}
+const environment = process.env.NODE_ENV;
+
+const production = {
+	apiUrl: "/api",
+};
+const development = {
+	apiUrl: "http://localhost:5000/api",
+};
+
+export default environment === "production" ? production : development;
