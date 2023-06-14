b5673479076f59cbb95af5dd59e50f6199dc7456
jaanonim <mat8mro@gmail.com>
2023-06-14 14:46:05 +0200

    fix config envs

--- client/src/Config/config.ts
+++ client/src/Config/config.ts
@@ -1,12 +1,12 @@
-const environment = process.env.ENV;
+const environment = import.meta.env.PROD;
 
 const production = {
     apiUrl: "/api",
-    socketUrl: `"https://${process.env.BASE_URL}/"`,
+    socketUrl: `"https://${import.meta.env.BASE_URL}/"`,
 };
 const development = {
     apiUrl: "http://localhost:5000/api",
     socketUrl: "http://localhost:5000/",
 };
 
-export default environment === "production" ? production : development;
+export default environment ? production : development;
