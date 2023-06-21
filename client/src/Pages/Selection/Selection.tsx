61ebac8a1db07e1aec554b4418c46a680af53045
jaanonim <mat8mro@gmail.com>
2023-06-21 12:18:04 +0200

    fix Selection page for mobile

--- client/src/Pages/Selection/Selection.tsx
+++ client/src/Pages/Selection/Selection.tsx
@@ -1,55 +1,68 @@
-import { Center, Container, Flex, Heading, Stack } from "@chakra-ui/react";
+import {
+    AbsoluteCenter,
+    Center,
+    Container,
+    Flex,
+    Heading,
+    Stack,
+} from "@chakra-ui/react";
 import ColorModeButton from "../../Components/ColorModeButton";
 import SelectionButton from "../../Components/SelectionButton";
 import TopRightCorner from "../../Components/TopRightCorner";
 import "./Selection.css";
 
 function Selection() {
-	return (
-		<div id="container">
-			<Container as="main">
-				<Center>
-					<Stack spacing={5}>
-						<Center>
-							<Heading as="h1" size="2xl">
-								SongBook
-							</Heading>
-						</Center>
-						<Center>
-							<Heading as="h2" size="md">
-								Select what you will do:
-							</Heading>
-						</Center>
-					</Stack>
-				</Center>
-				<Center>
-					<Flex>
-						<SelectionButton
-							color="orange"
-							url="/manage"
-							title="Manage"
-							subtitle="Manage your songs"
-						></SelectionButton>
-						<SelectionButton
-							color="blue"
-							url="/present"
-							title="Present"
-							subtitle="Present your songs"
-						></SelectionButton>
-						<SelectionButton
-							color="green"
-							url="/screen"
-							title="Screen"
-							subtitle="Screen for presentation"
-						></SelectionButton>
-					</Flex>
-				</Center>
-			</Container>
-			<TopRightCorner>
-				<ColorModeButton />
-			</TopRightCorner>
-		</div>
-	);
+    return (
+        <>
+            <Container as="main" w="100vw" h="100vh">
+                <AbsoluteCenter>
+                    <Center>
+                        <Stack spacing={5}>
+                            <Center>
+                                <Heading as="h1" size="2xl">
+                                    SongBook
+                                </Heading>
+                            </Center>
+                            <Center>
+                                <Heading as="h2" size="md">
+                                    Select what you will do:
+                                </Heading>
+                            </Center>
+                        </Stack>
+                    </Center>
+                    <Center>
+                        <Flex
+                            flexWrap="wrap"
+                            alignItems="center"
+                            justifyContent="center"
+                        >
+                            <SelectionButton
+                                color="orange"
+                                url="/manage"
+                                title="Manage"
+                                subtitle="Manage your songs"
+                            ></SelectionButton>
+                            <SelectionButton
+                                color="blue"
+                                url="/present"
+                                title="Present"
+                                subtitle="Present your songs"
+                            ></SelectionButton>
+                            <SelectionButton
+                                color="green"
+                                url="/screen"
+                                title="Screen"
+                                subtitle="Screen for presentation"
+                            ></SelectionButton>
+                        </Flex>
+                    </Center>
+                </AbsoluteCenter>
+            </Container>
+            <TopRightCorner>
+                <ColorModeButton />
+            </TopRightCorner>
+        </>
+    );
 }
 
 export default Selection;
